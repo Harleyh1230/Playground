006c8417ebda25273dd7506c18e19b4879658879
Harley Hutchinson <Harley.Hutchinson@inphase.com>
2024-01-17 14:33:17 +0000

    MDX Update

--- app/blog/[id]/page.jsx
+++ app/blog/[id]/page.jsx
@@ -1,5 +1,8 @@
+import "./styles.css"
 import { getBlogPost } from "@/lib/utils";
 import Link from "next/link";
+import { MDXRemote } from "next-mdx-remote/rsc";
+
 
 export default async function page({ params, searchParams }) {
   console.log(params);
@@ -8,6 +11,36 @@ export default async function page({ params, searchParams }) {
   const id = searchParams.BlogId; // read from pathname
 
   const BlogInfo = await getBlogPost({ id });
+
+  const MDX = `
+  # angular-course
+  
+  This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.0.5.
+  
+  ## Development server
+  
+  Run \`ng serve\` for a dev server. Navigate to \`http://localhost:4200/\`. The application will automatically reload if you change any of the source files.
+  
+  ## Code scaffolding
+  
+  Run \`ng generate component component-name\` to generate a new component. You can also use \`ng generate directive|pipe|service|class|guard|interface|enum|module\`. 
+  
+  ## Build
+  
+  \`ng build\` to build the project. The build artifacts will be stored in the \`dist/\` directory.
+  
+  ## Running unit tests
+  
+  Run \`ng test\` to execute the unit tests via [Karma](https://karma-runner.github.io).
+  
+  ## Running end-to-end tests
+  
+  Run \`ng e2e\` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
+  
+  ## Further help
+  
+  To get more help on the Angular CLI use \`ng help\` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+  `;
 
   return (
     <>
@@ -35,6 +68,11 @@ export default async function page({ params, searchParams }) {
             <p className="text-lg leading-7 min-h-40">{BlogInfo.body}</p>
           </div>
         </div>
+      </div>
+      <hr />
+      <div className="m-3 mb-4">
+        <p className="text-3xl">MDX</p>
+        <MDXRemote source={MDX} />
       </div>
     </>
   );
